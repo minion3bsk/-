f2b1e52fe47aaaa16d26718bf94463317f551931
Hase Mao24 <53070847+minion3bsk@users.noreply.github.com>
2021-09-26 20:14:08 +0900

    success

--- divided_num.c
+++ divided_num.c
@@ -2,35 +2,44 @@
 #include <math.h>
 
 int main(void){
-    int count = 0;
-    int i = 360;
-    int n = i;
-    int left = 0;
+    int i;
     int div;
-    int d_i = (double)i;
-    int sq = sqrt(d_i); 
-    printf("%d\n", i);
-    for(div=2;div<=sq;div++){
-        if(n != 0){
-            left = 0;
-            count = 0;
-            while(left == 0){
-                left = n % div;
-                if(left == 0){
-                    n = n / div;
+    for(i=2;i<=100;i++){
+        printf("%d = ", i);
+        int n = i;
+        int left = 0;
+        int d_i = (double)i;
+        int sq = sqrt(d_i); 
+            int div_count = 0;
+        for(div=2;div<=i;div++){
+            int count = 0;
+            if(n != 0){
+                left = 0;
+                count = 0;
+                while(left == 0){
+                    left = n % div;
+                    if(left == 0){
+                        n = n / div;
+                    }
+                    count++;
                 }
-                count++;
+                    count--;
+                    if(count!=0){
+                        if(div_count>=1){
+                            printf("・");
+                        }
+                        printf("%d^%d",div, count); 
+                        div_count++;
+                    }
+                    int d_count = (double)count;
+                    /* double div_n = pow(div, count);
+                    div_n = (int)div_n;
+                    n = i / div_n; */
             }
-                count--;
-                if(count!=0){
-                    printf(":%dで%d回\n",div, count); 
-                }
-                int d_count = (double)count;
-                /* double div_n = pow(div, count);
-                div_n = (int)div_n;
-                n = i / div_n; */
         }
+        printf("\n");
     }
+
 
 
 }
